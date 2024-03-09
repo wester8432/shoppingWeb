b8b812bec51d0ee0a4aca5df2a3bcf00350345be
wester8432 <wester8432@gmail.com>
2024-03-09 16:02:02 +0800

    git init

--- src/pages/front/Cart.js
+++ src/pages/front/Cart.js
@@ -1,4 +1,4 @@
-import { useOutletContext } from "react-router-dom";
+import { useOutletContext, Link } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 import { isDisabled } from "@testing-library/user-event/dist/utils";
@@ -97,12 +97,12 @@ export default function Cart() {
             <p className="mb-0 h4 fw-bold">總金額</p>
             <p className="mb-0 h4 fw-bold">NT${cartData.final_total}</p>
           </div>
-          <a
-            href="./checkout.html"
+          <Link
+            to="checkout"
             className="btn btn-dark w-100 mt-4 rounded-0 py-3"
           >
             確認餐點正確
-          </a>
+          </Link>
         </div>
       </div>
     </div>
