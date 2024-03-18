0048fce0009a0609013d89852d201f8785d84a68
wester8432 <wester8432@gmail.com>
2024-03-18 18:27:56 +0800

    git init

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -4,32 +4,33 @@ export default function SwiperImg() {
       <div
         id="carouselExampleControlsNoTouching"
         class="carousel slide"
-        data-bs-touch="false"
-        data-bs-interval="false"
+        data-bs-ride="carousel"
+        // data-bs-touch="false"
+        // data-bs-interval="true"
       >
         <div class="carousel-inner">
-          <div class="carousel-item active">
+          <div class="carousel-item active" data-bs-interval="5000">
             <img
               src="https://i.imgur.com/6zWwEaf.png"
               class="d-block w-100 objectFit"
               alt="..."
-              height="600px"
+              height="900px"
             />
           </div>
-          <div class="carousel-item">
+          <div class="carousel-item" data-bs-interval="5000">
             <img
               src="https://cdn.esence.travel/%E6%9C%AC%E8%B3%AA%E6%97%85%E8%A1%8C/%E6%9C%AC%E8%B3%AA%E6%97%85%E8%A1%8C-%E6%95%85%E4%BA%8B%E6%96%87%E7%AB%A0%E9%A6%96%E5%9C%96-1200737-1-5.png"
               class="d-block w-100 objectFit"
               alt="..."
-              height="600px"
+              height="900px"
             />
           </div>
-          <div class="carousel-item">
+          <div class="carousel-item" data-bs-interval="5000">
             <img
               src="https://i.pinimg.com/564x/04/dd/e2/04dde285b059b8cd0cf7267a62163bed.jpg"
               class="d-block w-100 objectFit"
               alt="..."
-              height="600px"
+              height="900px"
             />
           </div>
         </div>
